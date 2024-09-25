df7535cf510c3e3f44050bf95aef61737ce716b1
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-09-25 14:52:45 +0200

    first day

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "radwan",
+      status: 'pending',
+      tasks: ["taskone", "tasktwo", "taskthree"],
+    };
+  },
+  methods :{
+    toggleStatus(){
+      if(this.status==='active'){
+          this.status='pending';
+      }
+      else if (this.status==='pending')
+      {
+          this.status='inactive'
+      }
+      else if (this.status==='inactive')
+      {
+          this.status='active'
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>
+    {{ name }}
+  </h1>
+  <p v-if="status==='active'">User IS active...</p>
+  <p v-if="status==='pending'">User IS pending...</p>
+  <p v-if="status==='inactive'">User IS  inactive...</p>
+  <h3>Tasks:</h3>
+  <ui>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ui>
+  <!-- <br><hr> -->
+  <button v-on:click="toggleStatus"> Change Statues </button>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
